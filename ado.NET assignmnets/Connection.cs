347ef666c47118385f36cc7dd797b6dbb38bddde
Sandhyabcap <115027647+Sandhyabcap@users.noreply.github.com>
2022-11-17 10:43:11 +0530

    Add files via upload

---
+++ ado.NET assignmnets/Connection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+// provider namespace
+using System.Linq.Expressions;
+
+namespace connectingDB
+{
+    class program1
+    {
+        static void Main(String[] args)
+        {
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source = lsandyabVM; Initial Catalog = connect;Integrated Security = True";
+            //connection  string
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                Console.WriteLine("\n\n\n\n************connection established successfully***********");
+
+
+            }
+
+
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+        }
+
+    }
+}
