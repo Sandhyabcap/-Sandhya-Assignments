99f7bdea56288a6225fb5975a3177c5a8f71b25a
Sandhyabcap <115027647+Sandhyabcap@users.noreply.github.com>
2022-11-17 10:52:52 +0530

    Add files via upload

---
+++ ado.NET assignments/Insert.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPT
+{
+    internal class I
+    {
+
+        static void Main(string[] args)
+        {
+
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=lsandyabVM;Initial Catalog=connect;Integrated Security=True";
+
+            try
+            {
+
+
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                Console.WriteLine("\n\n\n*********connection established successfully*********");
+
+                Console.WriteLine("\n\n\n\nPlease Enter Employee Id : ");
+                int EId = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\nPlease Enter Employee First Name : ");
+                String Firstname = Console.ReadLine();
+
+                Console.WriteLine("\nPlease Enter Employee Last Name : ");
+                String Lastname = Console.ReadLine();
+
+                Console.WriteLine("\nPlease Enter Employee Salary : ");
+                String Salary = Console.ReadLine();
+
+
+                string insertQuery = "insert into Employees(EId, Firstname, Lastname , Salary) Values('" + EId + "','" + Firstname + "','" + Lastname + "','" + Salary + "') ";
+                SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection);
+                insertCommand.ExecuteNonQuery();
+                Console.WriteLine("Data Inserted");
+                sqlConnection.Close();
+
+            }
+
+
+            catch (Exception e)
+
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+        }
+
+    }
+}
---
+++ ado.NET assignments/Update.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ADO.net
+    {
+    internal class Update
+    {
+        public static void Main(string[] args)
+        {
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=lsandyabVM;Initial Catalog=connect;Integrated Security=True";
+
+            try
+            {
+
+
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                //update=>u
+                int u_EmpID;
+                string u_FirstName;
+                string u_LastName;
+                int u_Salary;
+                Console.WriteLine("enter EmpID that i like to update");
+                u_EmpID = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter FirstName that i like to update");
+                u_FirstName = Console.ReadLine();
+                Console.WriteLine("enter LastName that i like to update");
+                u_LastName = Console.ReadLine();
+                Console.WriteLine("enter Salary that i like to update");
+                u_Salary = int.Parse(Console.ReadLine());
+                string updateQuery = "update EmployeeData set FirstName = '" + u_FirstName + "', LastName = '" + u_LastName + "', Salary = '" + u_Salary + "' where EmpID ='" + u_EmpID + "'";
+                SqlCommand updateCommand = new SqlCommand(updateQuery, sqlConnection);
+                updateCommand.ExecuteNonQuery();
+                Console.WriteLine("Data updated successfully");
+                Console.ReadLine();
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
